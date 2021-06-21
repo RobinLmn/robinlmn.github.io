41fe6bce8a2331296a5a22d41fae3332f99e82b7
RobinLmn <55120018+RobinLmn@users.noreply.github.com>
2021-06-21 14:46:41 -0400

    Updated intro and resume

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -3,29 +3,27 @@
     <div class="intro">
       Hi! Welcome to my website! <br><br>
       As a current student at McGill University in Physics and Computer Science, I am passionate about information technology and problem-solving. 
-      I love puzzles and challenges, so if you have an unsolvable enigma for me, I am your guy! 
+      My current interests are centered around game development, especially real time computer animation and physics simulation.
       <br><br>
       Before I go on, and because I am sure that is what you are interested in, here is my <a href="https://robinlmn.github.io/pdf/robin-leman-resume.pdf" target="_blank">Resume</a>.
       <br><br>
       I am curious and eager to learn: always looking for new experiences and new skills to acquire. If you are looking for me, you will probably find me on youtube 
       watching new tutorials or reading science blogs and Wikipedia pages. 
       <br><br>
-      I love  <a href="https://github.com/filrod/UbisoftGameLabMcGill_2" target="_blank">game development</a> to the point where I prefer making games than playing them. I created 
+      I love  <a href="https://github.com/filrod/UbisoftGameLabMcGill_2" target="_blank">game development</a> to the point where I prefer making games than playing them. I have more and
+      more specialized in computer animation and physics simulation. That is why I created
       <a href="https://robinlmn.github.io/achilles.html" target="_blank">Achilles</a>, a 3D Physics Engine in ECS, with translational and rotational dynamics, collision detection and 
       response, and a simple spring system for softbody simulation.
       <br><br>
-      I'm passionate about <a href="https://github.com/RobinLmn/md5-decryption-crackingchallenge" target="_blank">cybersecurity</a>: it probably comes from my brother, 
-      who used to design me hacking challenges and bought me a textbook on ethical hacking when I was ten.
-      <br><br>
       More than ever, I think <a href="https://github.com/RobinLmn/McHacks7_re-ycle" target="_blank">sustainability and environment preservation</a> are crucial to our world. 
-      I like having an impact on my work and will always look into having
-      a positive influence on the planet.
+      I like my work to make an impact and will always look into having a positive influence on the planet.
       <br><br>
       I would love to connect with you, either on <a href="https://www.linkedin.com/in/robin-leman-a5043816b/" target="_blank">LinkedIn</a> or 
-      by <a href="mailto:robin.leman@mail.mcgill.ca" target="_blank">Mail</a>, so don't hesitate to reach out!
+      by <a href="mailto:robin.leman@mail.mcgill.ca" target="_blank">e-mail</a>, so don't hesitate to reach out!
       <br><br><br><br>
       <i> This website has been made from scratch with Vue.js by me. You can find its source code <a href="https://github.com/RobinLmn/robinlmn.github.io" target="_blank">here</a></i>.
-    </div>
+      <br>
+   </div>
   </introtext>
 </template>
 
